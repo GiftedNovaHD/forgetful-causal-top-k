46f0531e0d78443446d84f7511b2113ebde326b7
PuR3Luck <aidan.ong822@gmail.com>
2025-01-30 23:29:45 +0800

    implememtn top k mask

--- mask.py
+++ mask.py
@@ -1,10 +1,13 @@
 import torch.nn as nn
-from torch.nn.attention.flex_attention import _score_mod_signature, or_masks, and_masks
+from torch.nn.attention.flex_attention import or_masks, and_masks
 
 """
   Notes for coding masks with flex attention:
     - The 2 tokens will be allowed to attention to eachother if the mask returns True
 """
+
+def causal_mask(b, h, q_idx, kv_idx):
+  return q_idx >= kv_idx
 
 def sliding_window_mask_constructor(window_size):
   """
@@ -20,10 +23,18 @@ def sliding_window_mask_constructor(window_size):
   sliding_window_mask = and_masks(sliding_window,causal_mask)
   
   return sliding_window_mask
-  
 
-def causal_mask(b, h, q_idx, kv_idx):
-  return q_idx >= kv_idx
+def inverse_sliding_window_mask_constructor(window_size):
+  """
+    Generate an inverse mask for sliding window attention given a window size.
+  """
+
+  def inverse_sliding_window(b, h, q_idx, kv_idx):
+    return q_idx - kv_idx >= window_size
+  
+  inverse_sliding_window_mask = and_masks(inverse_sliding_window,causal_mask)
+
+  return inverse_sliding_window_mask
 
 def persistent_mask_constructor(num_persistent_tokens):
   """
@@ -36,6 +47,19 @@ def persistent_mask_constructor(num_persistent_tokens):
     return kv_idx < num_persistent_tokens
   
   return persistent
+
+
+def inverse_persistent_mask_constructor(num_persistent_tokens):
+  """
+    Generate an inverse mask for persistent tokens.
+
+    Args:
+      num_persistent_tokens: int, the number of persistent tokens
+  """
+  def inverse_persistent(b, h, q_idx, kv_idx):
+    return kv_idx >= num_persistent_tokens
+
+  return inverse_persistent
 
 def swa_persistent_mask_constructor(window_size, num_persistent_tokens):
   """
@@ -50,3 +74,18 @@ def swa_persistent_mask_constructor(window_size, num_persistent_tokens):
   swa_persistent = or_masks(swa_mask, persistent_mask)
 
   return swa_persistent
+
+def top_k_mask_constructor(window_size, num_persistent_tokens, ):
+  """
+    Generate a mask for tokens which top_k attention will be applied to
+  Args:
+      window_size: int, the size of the sliding window
+      num_persistent_tokens: int, the number of persistent tokens
+  """
+  
+  inverse_persistent_mask = inverse_persistent_mask_constructor(num_persistent_tokens)
+  inverse_sliding_window_mask = inverse_sliding_window_mask_constructor(window_size)
+
+  top_k_mask = and_masks(inverse_persistent_mask, inverse_sliding_window_mask)
+
+  return top_k_mask
--- visualise_mask.py
+++ visualise_mask.py
@@ -1,8 +1,15 @@
+import torch
+from torch.nn.attention.flex_attention import _score_mod_signature, or_masks, and_masks
+
 from mask import (sliding_window_mask_constructor,
                   persistent_mask_constructor,
-                  swa_persistent_mask_constructor
+                  swa_persistent_mask_constructor,
+                  top_k_mask_constructor,
+                  inverse_persistent_mask_constructor,
+                  inverse_sliding_window_mask_constructor
                  )
-import torch
+
+from score_mod import (tanh_softcap_constructor)
 
 def main(device: str = "cpu"):
     """Visualize the attention scores of sliding window mask mod.
@@ -16,15 +23,27 @@ def main(device: str = "cpu"):
 
     window_size = 4
     num_persistent_tokens = 2
+    tanh_softcap = 30
 
     def make_tensor():
         return torch.ones(B, H, SEQ_LEN + num_persistent_tokens, HEAD_DIM, device=device)
 
     query, key = make_tensor(), make_tensor()
+    swa_mask = sliding_window_mask_constructor(window_size)
+    p_mask = persistent_mask_constructor(num_persistent_tokens)
+    swa_p_mask = swa_persistent_mask_constructor(window_size, num_persistent_tokens)
+    inverse_swa_mask = inverse_sliding_window_mask_constructor(window_size)
+    inverse_p_mask = inverse_persistent_mask_constructor(num_persistent_tokens)
+    top_k_mask = top_k_mask_constructor(window_size, num_persistent_tokens)
+    score_mod =  tanh_softcap_constructor(tanh_softcap)
 
-    mask = swa_persistent_mask_constructor(window_size, num_persistent_tokens)
     visualize_attention_scores(
-        query, key, mask_mod=mask, device=device, name=f"swa_{window_size}_persistent_{num_persistent_tokens}_mask"
+        query,
+        key,
+        score_mod=None,
+        mask_mod=top_k_mask,
+        device=device,
+        name=f"topk_swa_{window_size}_persistent_{num_persistent_tokens}_mask"
     )
 
 
